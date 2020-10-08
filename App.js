4b951933812d7e873014bf9b68283f39928ea899
abdulrahman almutairi <dahem@abdulrahmans-Mac.local>
2020-10-07 17:44:47 -0700

    eject and  Update react native to 0.63.3

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 
 import { I18nManager,StyleSheet, Text, View } from 'react-native';
 import store from "./src/store/";
 import { Provider } from "react-redux";
+import SplashScreen from 'react-native-splash-screen';
 // import i18n from "./i18n";
 import { AppProvider } from "./src/utils/AppContext";
 import App from './src/navigation'
 // i18n.locale = "ar";
+
 import GlobalComponent from "./src/components/GlobalComponent"
 I18nManager.forceRTL(false);
 I18nManager.allowRTL(false);
@@ -15,7 +17,9 @@ export default function Min() {
     strings: "i18n",
     name: 'sami'
   };
-
+    useEffect(() => {
+        SplashScreen.hide();
+     }, []);
   
   return (
 
---
+++ metro.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  transformer: {
+    assetPlugins: ['expo-asset/tools/hashAssetFiles'],
+  },
+  // server: {
+  //   port: 8082,
+  // },
+};
